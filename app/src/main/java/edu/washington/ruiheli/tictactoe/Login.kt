cd21fd79976dcd5ed0a2018a3b61be109052f2eb
Ruihe (Jerry) Li <ruiheli@uw.edu>
2018-06-01 23:06:37 -0700

    sign up layout, and link from sign in

--- app/src/main/java/edu/washington/ruiheli/tictactoe/Login.kt
+++ app/src/main/java/edu/washington/ruiheli/tictactoe/Login.kt
@@ -1,12 +1,20 @@
 package edu.washington.ruiheli.tictactoe
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var toSignUp = findViewById<Button>(R.id.toSignUpBtn)
+        toSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            this.startActivity(intent)
+        }
     }
 }
