936362b7bac5d708ca5d737d1e594ff99ec4a720
isaacJChen <isaacjc@uw.edu>
2018-06-03 15:19:14 -0700

    set up all screens

---
+++ app/src/main/java/edu/washington/ruiheli/tictactoe/OnlineGameBoard.kt
@@ -0,0 +1,12 @@
+package edu.washington.ruiheli.tictactoe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OnlineGameBoard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_online_game_board)
+    }
+}
