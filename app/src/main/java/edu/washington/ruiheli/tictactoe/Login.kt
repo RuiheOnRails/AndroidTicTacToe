7ae8f9fa50768a18c68e16cd6237707a307fed69
isaacJChen <isaacjc@uw.edu>
2018-06-03 16:53:19 -0700

    can signin through firebase now

--- app/src/main/java/edu/washington/ruiheli/tictactoe/Login.kt
+++ app/src/main/java/edu/washington/ruiheli/tictactoe/Login.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,27 @@ class Login : AppCompatActivity() {
         toSignUp.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             this.startActivity(intent)
+        }
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        val email = findViewById<TextView>(R.id.loginEmailEditText)
+        val password = findViewById<TextView>(R.id.loginPasswordEditText)
+
+        loginBtn.setOnClickListener {
+            if (email.text.isEmpty()){
+                Toast.makeText(this, "email cannot be empty", Toast.LENGTH_SHORT).show()
+            } else if (password.text.isEmpty()) {
+                Toast.makeText(this, "password cannot be empty", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener {task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, MainMenu::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "sign in failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
