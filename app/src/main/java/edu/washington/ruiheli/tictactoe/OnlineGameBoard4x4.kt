28eb7f251af4c40bc0248ff6067c265ba00304bb
Ruihe (Jerry) Li <ruiheli@uw.edu>
2018-06-03 17:05:49 -0700

    online and offline 4x4

---
+++ app/src/main/java/edu/washington/ruiheli/tictactoe/OnlineGameBoard4x4.kt
@@ -0,0 +1,12 @@
+package edu.washington.ruiheli.tictactoe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OnlineGameBoard4x4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_online_game_board4x4)
+    }
+}
