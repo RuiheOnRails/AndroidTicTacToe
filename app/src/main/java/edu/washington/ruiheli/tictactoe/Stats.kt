5155d56d2c598eaedf8916385949e775646b2384
uwd73d8g <tina.zhuang.t.z@gmail.com>
2018-06-04 18:01:56 -0700

    modified stat

--- app/src/main/java/edu/washington/ruiheli/tictactoe/Stats.kt
+++ app/src/main/java/edu/washington/ruiheli/tictactoe/Stats.kt
@@ -20,36 +20,33 @@ class Stats : AppCompatActivity() {
 
         var auth = FirebaseAuth.getInstance()
         val rootRef = FirebaseDatabase.getInstance().reference
-        val userRef = rootRef.child("users")
-        var user = auth.currentUser!!.uid   // current user id
-        val eventListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for (ds in dataSnapshot.children) {
-                    if (user == ds.key) {  // find current user in our user database? Does .key return id?
-                        val lost = ds.child("gamesLost").getValue(Int::class.java)
-                        Log.d("TAG", lost.toString())
-                        val play = ds.child("gamesPlayed").getValue(Int::class.java)
-                        Log.d("TAG", play.toString())
-                        val tie = ds.child("gamesTied").getValue(Int::class.java)
-                        Log.d("TAG", tie.toString())
-                        val won = ds.child("gamesWon").getValue(Int::class.java)
-                        Log.d("TAG", won.toString())
-                        var ratio = findViewById<TextView>(R.id.stat)
-                        ratio.text = "$won vs. $lost"
-                        var lostBtn = findViewById<TextView>(R.id.lost)
-                        var playBtn = findViewById<TextView>(R.id.played)
-                        var tieBtn = findViewById<TextView>(R.id.tied)
-                        var wonBtn = findViewById<TextView>(R.id.won)
-                        lostBtn.append(lost.toString())
-                        playBtn.append(play.toString())
-                        tieBtn.append(tie.toString())
-                        wonBtn.append(won.toString())
-                    }
-                }
-
+        var id = auth.currentUser!!.uid   // current user id
+        val userRef = rootRef.child("users").child(id)
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(ds: DataSnapshot) {
+                val lost = ds.child("gamesLost").getValue(Int::class.java)
+                Log.d("TAG", lost.toString())
+                val play = ds.child("gamesPlayed").getValue(Int::class.java)
+                Log.d("TAG", play.toString())
+                val tie = ds.child("gamesTied").getValue(Int::class.java)
+                Log.d("TAG", tie.toString())
+                val won = ds.child("gamesWon").getValue(Int::class.java)
+                Log.d("TAG", won.toString())
+                var ratio = findViewById<TextView>(R.id.stat)
+                ratio.text = "$won vs. $lost"
+                var lostBtn = findViewById<TextView>(R.id.lost)
+                var playBtn = findViewById<TextView>(R.id.played)
+                var tieBtn = findViewById<TextView>(R.id.tied)
+                var wonBtn = findViewById<TextView>(R.id.won)
+                lostBtn.append(lost.toString())
+                playBtn.append(play.toString())
+                tieBtn.append(tie.toString())
+                wonBtn.append(won.toString())
             }
-            override fun onCancelled(databaseError: DatabaseError) {}
-        }
-        userRef.addListenerForSingleValueEvent(eventListener)
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("The read failed: " + databaseError.code)
+            }
+        })
     }
 }
