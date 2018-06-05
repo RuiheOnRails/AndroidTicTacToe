fcc16f30091d358b2c362e8bb0271a339d9ac539
uwd73d8g <tina.zhuang.t.z@gmail.com>
2018-06-04 17:40:21 -0700

    add stat screen

--- app/src/main/java/edu/washington/ruiheli/tictactoe/MainMenu.kt
+++ app/src/main/java/edu/washington/ruiheli/tictactoe/MainMenu.kt
@@ -1,7 +1,9 @@
 package edu.washington.ruiheli.tictactoe
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainMenu : AppCompatActivity() {
 
@@ -10,6 +12,13 @@ class MainMenu : AppCompatActivity() {
         setContentView(R.layout.activity_main_menu)
 
         val actionBar = supportActionBar
+
+        var stat = findViewById<Button>(R.id.stats)
+        stat.setOnClickListener {
+            val intent = Intent(this, Stats::class.java)
+            this.startActivity(intent)
+        }
+
 
     }
 }
